249bc1eeb3b031e9764062d0f8b3a844b1c81ab9
Julie Jonak <juliejonak10@gmail.com>
2018-11-03 16:14:27 -0700

    Added toml file for netlify deployment

--- gatsby-site/src/pages/list.js
+++ gatsby-site/src/pages/list.js
@@ -1,15 +1,26 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import ListItem from '../components/ListItem';
 
 
 class ReviewList extends React.Component {
     constructor(){
         super();
+        this.state={
+            groceries: [ {item: "banana", quantity: 3}, {item: "orange", quantity: 1}, ]
+        }
     }
 
     render(){
         return(
             <div>
+                <h1>Recipe Shopper</h1>
+                <h3>Your Basket</h3>
+                <div className="item-list">
+                    {this.state.groceries.map(item =>{
+                        return <ListItem obj={item} key={Date.now()} />
+                    })}
+                </div>
                 <h1>Get your groceries</h1>
                 <Link to="/checkout">Checkout</Link>
             </div>
@@ -17,6 +28,8 @@ class ReviewList extends React.Component {
     }
 }
     
+//map the groceries array, each item.item is the <p> and item.quantity is the auto fill of the input
+
 
 
 export default ReviewList;
