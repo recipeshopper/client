3eea77334f7c96fc37775096da5bcc2702daf9c4
Roland <rolandcanuto@outlook.com>
2018-11-03 18:19:50 -0700

    trying to add the functions

--- gatsby-site/functions/controller.js
+++ gatsby-site/functions/controller.js
@@ -4,6 +4,8 @@ require('dotenv').config();
 const q = faunadb.query
 const client = new faunadb.Client({ secret: process.env.FAUNADB_SERVER_SECRET })
 
+console.log(client);
+
 function hello(event, context, callback) {
     callback(null, {
       statusCode: 200,
