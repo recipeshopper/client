55e7dd9d07cdf7cb5624366c7442ff5b1c6d5c66
rebz135 <rebz135@gmail.com>
2018-11-03 19:15:12 -0700

    Complete parser script

--- gatsby-site/src/components/parser.js
+++ gatsby-site/src/components/parser.js
@@ -1,7 +1,5 @@
 var parser = require('ingredients-parser');
 
-console.log('PARSER LOADED');
-
 let getIngredients = () => {
 	let ingrArr = [];
 	let temp = document.getElementsByClassName("recipe-ingred_txt");
@@ -10,26 +8,32 @@ let getIngredients = () => {
 			ingrArr.push(temp[i].textContent);
 		}
 	}
-	return getIngredients;
+	return ingrArr;
 }
 
-//DUMMY DATA
+//DUMMY DATA - REMOVE WHEN DEPLOYING
 var dummyData = ["2 cups all-purpose flour", "1/2 teaspoon baking soda", "1/2 teaspoon salt", "4 3/4 cup unsalted butter, melted", "1 cup packed brown sugar", "1/2 cup white sugar", "1 tablespoon vanilla extract", "1 egg", "1 egg yolk", "2 cups semisweet chocolate chips"];
 
 let stringToNum = (string) => {
-//TO DO
+	return temp = string.split(' ').reduce((accum, ele)=>{
+		return accum + eval(ele);
+	}, 0)
 }
 
 let ingredParser = (ingrArr) => {
 	let ingrObj = [];
 	ingrArr.forEach((ele)=>{
 		let tempObj = parser.parse(ele);
-		ingrObj.push(parser.parse(ele));
-
+		tempObj.amount = stringToNum(tempObj.amount);
+		ingrObj.push(tempObj);
 	})
 	return ingrObj;
 }
 
 console.log(ingredParser(dummyData));
 
-//TODO
+let scrapeIngredients = () => {
+	let ingrArr = getIngredients();
+	let result = ingredParser(ingrArr);
+	return result;
+}
