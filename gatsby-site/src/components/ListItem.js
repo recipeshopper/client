160e09c60b46f09b0426dd9272ca24367894ad64
kyranmccann <kyran.mccann@gmail.com>
2018-11-03 22:02:50 -0700

    listItem update

--- gatsby-site/src/components/ListItem.js
+++ gatsby-site/src/components/ListItem.js
@@ -1,10 +1,6 @@
 import React from 'react';
-<<<<<<< HEAD
 import { connect } from 'react-redux';
 import { Form, Input, Label, DeleteSpan, Wrapper } from './styledComponents';
-=======
-import { Form, Input } from './styledComponents';
->>>>>>> b1dc53e9692d120875ad5229abdb9679a5e98c1a
 
 const ListItem = (props) => {
     const input = (e) => {
@@ -13,7 +9,6 @@ const ListItem = (props) => {
     }
 
         return(
-<<<<<<< HEAD
             <Wrapper>
                 <Form>
                     <Label>
@@ -21,16 +16,6 @@ const ListItem = (props) => {
                     {props.ingredient}
                     <DeleteSpan onClick={props.deleter}>X</DeleteSpan>
                     </Label>
-=======
-            <div>
-                <Form> 
-                    <label> 
-                    <Input type="number" defaultValue={props.amount} onChange={input}></Input>
-                    {props.ingredient}
-                    <h2>current state: {props.amount}</h2>
-                    <span onClick={props.delete}>X</span>
-                    </label>
->>>>>>> b1dc53e9692d120875ad5229abdb9679a5e98c1a
                 </Form>
             </Wrapper>
         )
